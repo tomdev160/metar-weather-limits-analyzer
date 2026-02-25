2096dd38c7e21e4ef620c6a20201dc9c63e3a21e
tomdev160 <tomdevaal@outlook.com>
2026-02-25 14:02:49 +0100

    Fix syntax error by removing the extra parenthesis

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default {
+
+export default defineConfig({
   base: '/metar-weather-limits-analyzer/',
   plugins: [react()],
 })
