bcd02c7b684f08ac605183c89ec25e14d6cf08bc
tomdev160 <tomdevaal@outlook.com>
2026-02-25 13:57:18 +0100

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
+export default {
+  base: '/metar-weather-limits-analyzer/',
   plugins: [react()],
 })
